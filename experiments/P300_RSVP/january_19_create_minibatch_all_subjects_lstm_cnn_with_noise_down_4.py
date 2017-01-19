992051b17a78c8f20fdfee65b019ac9d32d5142f
ORI <ori226@gmail.com>
2017-01-19 23:34:09 +0200

    add all_excpet_one experiment of CNN

--- experiments/P300_RSVP/january_19_create_minibatch_all_subjects_lstm_cnn_with_noise_down_4.py
+++ experiments/P300_RSVP/january_19_create_minibatch_all_subjects_lstm_cnn_with_noise_down_4.py
@@ -243,7 +243,7 @@ if __name__ == "__main__":
 
             break
     eeg_sample_shape = (50, 55)
-    only_p300_model_1 = get_only_P300_model_CNN(eeg_sample_shape)
+    only_p300_model_1 = get_only_P300_model_LSTM_CNN(eeg_sample_shape)
     only_p300_model_1.summary()
 
     from keras.optimizers import RMSprop
